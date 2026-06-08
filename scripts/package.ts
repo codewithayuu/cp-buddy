450e377f22ab8f85d13f2257d52b28c7d2719a5f
Ayush Jha <codewithayuu@gmail.com>
2026-06-08 19:59:26 +0530

    run webpack from root to fix resolution

--- scripts/package.ts
+++ scripts/package.ts
@@ -14,10 +14,10 @@ const copyDirFiles = (src: string, dst: string, ext?: string) => {
 run('pnpm clean');
 
 for (const pkg of ['vscode-webview', 'local-router']) {
-  run(`cd packages/${pkg} && pnpm compile`);
+  run('pnpm exec webpack --mode production --config packages/' + pkg + '/webpack.config.ts');
   copyDirFiles(`packages/${pkg}/dist`, 'packages/vscode-ext/dist');
 }
-run('cd packages/vscode-ext && pnpm compile');
+run('pnpm exec webpack --mode production --config packages/vscode-ext/webpack.config.ts');
 
 run('cd packages/browser-ext && pnpm compile');
 run('cd packages/vscode-ext && pnpm package');
